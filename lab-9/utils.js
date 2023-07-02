6aee535c58fd9b6d88e98ae15f97360b4467158c
Kevin Hawkins <kevin.hawkins@utp.ac.pa>
2023-07-02 16:37:02 -0500

    cache timer

--- lab-9/utils.js
+++ lab-9/utils.js
@@ -1,30 +1,35 @@
 const cache = require("./cache");
 const { get } = require("./remote");
 
+function getCurrentTimestamp() {
+  return Math.floor(Date.now() / 1000);
+}
+
+function isCacheEntryValid(cacheItem) {
+  const currentTimestamp = getCurrentTimestamp();
+  return cacheItem.timestamp + 300 >= currentTimestamp;
+}
+
 module.exports = {
   pokemon: {
-
     getCache() {
       return cache;
     },
-
     getPokemonStructure({ pokemon, fromCache = false }) {
       return {
         fromCache,
         pokemon,
       };
     },
-
     getPokemonUrl(pokemon) {
       console.log('getPokemonUrl');
       return `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
     },
-
     async get(pokemonId) {
       let fromCache, pokemon;
       console.log('GET POKEMON ID: ', pokemonId);
       console.log('POKEMON CACHE: ', cache[pokemonId]);
-      if (cache[pokemonId]) {
+      if (cache[pokemonId]  && isCacheEntryValid(cache[pokemonId])) {
         pokemon = cache[pokemonId];
         fromCache = true;
       } else {
@@ -41,7 +46,18 @@ module.exports = {
     },
 
     set(pokemonId, data) {
-      cache[pokemonId] = data;
+      cache[pokemonId] = {
+        data,
+        timestamp: getCurrentTimestamp(),
+      };
+      setTimeout(() => {
+        this.delete(pokemonId);
+      },  5 * 60 * 1000);
+    },
+
+    delete(pokemonId) {
+      delete cache[pokemonId];
+      console.log(`Pokemon deleted: ${pokemonId}`);
     },
   },
 };
